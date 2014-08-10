a378431986c1df3f2203b69ee1567838e30327d3
Jonatan Lund <jonatan.lund.92@gmail.com>
2014-08-11 00:37:56 +0200

    Added more dependencies

--- app/config.js
+++ app/config.js
@@ -8,12 +8,20 @@ require.config({
 		"marionette": "../bower_components/marionette/lib/core/backbone.marionette",
 		"backbone.wreqr" : "../bower_components/backbone.wreqr/lib/backbone.wreqr",
 		"backbone.babysitter" : "../bower_components/backbone.babysitter/lib/backbone.babysitter",
+		"backbone.supermodel" : "../bower_components/supermodel/supermodel"
 	},
 
 	shim: {
+		"backbone" : {
+			deps: ["jquery", "underscore"]
+		},
 		"marionette": {
 			deps: ["backbone.wreqr", "backbone.babysitter"],
 			exports: "marionette"
+		},
+		"backbone.supermodel" : {
+			deps: ["backbone", "underscore"],
+			exports: "Supermodel"
 		}
 	},
 
